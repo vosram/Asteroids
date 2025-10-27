1b49fa2ead86f66077b42a390f112d43caafa8b4
Walter Fritsche <63881577+vosram@users.noreply.github.com>
2025-10-27 15:05:25 -0500

    Add basic player to game

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ from constants import (SCREEN_WIDTH,
     ASTEROID_KINDS,
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS)
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -21,6 +23,7 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 fps
