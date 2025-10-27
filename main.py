6e8d716d80ee4bef18eb1f2ee5785db175d0bdc0
Walter Fritsche <63881577+vosram@users.noreply.github.com>
2025-10-27 09:30:06 -0500

    Remove unnecessary print statements

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
